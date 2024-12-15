6a54a0ec6dfac6206a1c3616e8a9e93f131a6fcc
Ose simeon <119625623+Dev-simeon-tech@users.noreply.github.com>
2024-12-16 00:51:26 +0100

    modified codebase with prettier

--- src/tab-slide.js
+++ src/tab-slide.js
@@ -1,24 +1,23 @@
-const tabSlideCont = document.querySelector('.tab-list-slide');
-const tabSlide = document.querySelectorAll('.tab-slide');
+const tabSlideCont = document.querySelector(".tab-list-slide");
+const tabSlide = document.querySelectorAll(".tab-slide");
 
-tabSlide.forEach( (tab,index) => {
-    tab.addEventListener('click',(e)=>{
-        changeActiveTab(tab)
-        updateSlide(index)
-    })
-    
+tabSlide.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    changeActiveTab(tab);
+    updateSlide(index);
+  });
 });
 
-function changeActiveTab(tab){
-    const currentab = tabSlideCont.querySelector('[aria-selected = "true"]')
-    const targetTab = tab
-    currentab.setAttribute('aria-selected',false);
-    targetTab.setAttribute('aria-selected',true);
+function changeActiveTab(tab) {
+  const currentab = tabSlideCont.querySelector('[aria-selected = "true"]');
+  const targetTab = tab;
+  currentab.setAttribute("aria-selected", false);
+  targetTab.setAttribute("aria-selected", true);
 
-    const offsetWidth = tab.offsetWidth;
-    const offsetLeft = tab.offsetLeft;
+  const offsetWidth = tab.offsetWidth;
+  const offsetLeft = tab.offsetLeft;
 
-    tabSlideCont.style.setProperty( '--indicator-width', `${offsetWidth}px`)
-    tabSlideCont.style.setProperty( '--indicator-offset', `${offsetLeft}px`)
+  tabSlideCont.style.setProperty("--indicator-width", `${offsetWidth}px`);
+  tabSlideCont.style.setProperty("--indicator-offset", `${offsetLeft}px`);
 }
-changeActiveTab(tabSlide[0])
+changeActiveTab(tabSlide[0]);
